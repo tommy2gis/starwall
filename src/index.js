b5b7f82f0408931267e7dd9f2b90508646019f45
淘米 <shitao19880316@gmail.com>
2021-12-04 16:28:54 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,18 @@
+/*
+ * @Author: shitao
+ * @Date: 2021-10-19 21:19:18
+ * @LastEditTime: 2021-10-24 12:59:44
+ * @LastEditors: shitao
+ * @Description: 
+ * @FilePath: \ywhzapp\src\index.js
+ * 无锡四维时空信息科技有限公司
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App.js';
+
+ReactDOM.render(
+    <App />,
+  document.getElementById('root')
+);
