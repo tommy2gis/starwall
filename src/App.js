1b7ea3483441630d94c34425a8556b42630b9d58
淘米 <shitao19880316@gmail.com>
2021-12-04 20:51:51 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,45 +1,69 @@
 /*
  * @Author: shitao
  * @Date: 2021-10-19 21:19:18
- * @LastEditTime: 2021-12-04 16:27:48
+ * @LastEditTime: 2021-12-04 20:34:37
  * @LastEditors: shitao
- * @Description: 
+ * @Description:
  * @FilePath: \starwall\src\App.js
  * 无锡四维时空信息科技有限公司
  */
-import QueueAnim from 'rc-queue-anim';
-import PropTypes from 'prop-types';
-import TweenOne, { TweenOneGroup } from 'rc-tween-one';
-import './App.css';
+import React from "react";
+import QueueAnim from "rc-queue-anim";
+import PropTypes from "prop-types";
+import TweenOne, { TweenOneGroup } from "rc-tween-one";
+import "./App.css";
 
 const textData = {
-  content: 'Taiwan called motorcycle, motor bike [1] or a motorcycle,' +
-  ' the motorcycle referred to in the mainland, Hong Kong and Southeast' +
-  ' Asia known as motorcycles.',
-  title: 'Motorcycle',
+  content: (
+    <>
+      <p>少年负壮气，奋烈自有时。</p>
+      <p>胸有惊雷，面如平湖。</p>
+      <p>带刀能防守，球队做榜样；</p>
+      <p>掉刀传球稳，突进亦能浪，</p>
+      <p>若问他是谁，国际颜三胖。</p>
+    </>
+  ),
+  title: "颜可达（掉刀小侍卫）",
 };
 let dataArray = [
-  { image: 'https://zos.alipayobjects.com/rmsportal/DGOtoWASeguMJgV.png' },
-  { image: 'https://zos.alipayobjects.com/rmsportal/BXJNKCeUSkhQoSS.png' },
-  { image: 'https://zos.alipayobjects.com/rmsportal/TDIbcrKdLWVeWJM.png' },
-  { image: 'https://zos.alipayobjects.com/rmsportal/SDLiKqyfBvnKMrA.png' },
-  { image: 'https://zos.alipayobjects.com/rmsportal/UcVbOrSDHCLPqLG.png' },
-  { image: 'https://zos.alipayobjects.com/rmsportal/QJmGZYJBRLkxFSy.png' },
-  { image: 'https://zos.alipayobjects.com/rmsportal/PDiTkHViQNVHddN.png' },
-  { image: 'https://zos.alipayobjects.com/rmsportal/beHtidyjUMOXbkI.png' },
-  { image: 'https://zos.alipayobjects.com/rmsportal/vJcpMCTaSKSVWyH.png' },
-  { image: 'https://zos.alipayobjects.com/rmsportal/dvQuFtUoRmvWLsZ.png' },
-  { image: 'https://zos.alipayobjects.com/rmsportal/QqWQKvgLSJaYbpr.png' },
-  { image: 'https://zos.alipayobjects.com/rmsportal/pTfNdthdsUpLPLJ.png' },
+  { image: "./starwall/images/1.jpg" },
+  { image: "./starwall/images/1.jpg" },
+  { image: "./starwall/images/1.jpg" },
+  { image: "./starwall/images/1.jpg" },
+  { image: "./starwall/images/1.jpg" },
+  { image: "./starwall/images/1.jpg" },
+  { image: "./starwall/images/1.jpg" },
+  { image: "./starwall/images/1.jpg" },
+  { image: "./starwall/images/1.jpg" },
+  { image: "./starwall/images/1.jpg" },
+  { image: "./starwall/images/1.jpg" },
+  { image: "./starwall/images/1.jpg" },
+  { image: "./starwall/images/1.jpg" },
+  { image: "./starwall/images/1.jpg" },
+  { image: "./starwall/images/1.jpg" },
+  { image: "./starwall/images/1.jpg" },
+  { image: "./starwall/images/1.jpg" },
+  { image: "./starwall/images/1.jpg" },
+  { image: "./starwall/images/1.jpg" },
+  { image: "./starwall/images/1.jpg" },
+  { image: "./starwall/images/1.jpg" },
+  { image: "./starwall/images/1.jpg" },
+  { image: "./starwall/images/1.jpg" },
+  { image: "./starwall/images/1.jpg" },
+  { image: "./starwall/images/1.jpg" },
+  { image: "./starwall/images/1.jpg" },
+  { image: "./starwall/images/1.jpg" },
+  { image: "./starwall/images/1.jpg" },
+  { image: "./starwall/images/1.jpg" },
 ];
-dataArray = dataArray.map(item => ({ ...item, ...textData }));
+dataArray = dataArray.map((item) => ({ ...item, ...textData }));
 class PicDetails extends React.Component {
   static propTypes = {
     className: PropTypes.string,
   };
 
   static defaultProps = {
-    className: 'pic-details-demo',
+    className: "pic-details-demo",
   };
 
   constructor(props) {
@@ -79,51 +103,55 @@ class PicDetails extends React.Component {
   };
 
   getDelay = (e) => {
-    const i = e.index + dataArray.length % 4;
-    return (i % 4) * 100 + Math.floor(i / 4) * 100 + 200;
+    const i = e.index + (dataArray.length % 10);
+    return (i % 10) * 100 + Math.floor(i / 10) * 100 + 200;
   };
 
   getLiChildren = () => {
     const imgWidth = 110;
-    const imgHeight = 76;
+    const imgHeight = 176;
     const imgBoxWidth = 130;
-    const imgBoxHeight = 96;
+    const imgBoxHeight = 196;
     return dataArray.map((item, i) => {
       const { image, title, content } = item;
-      const isEnter = typeof this.state.picOpen[i] === 'boolean';
+      const isEnter = typeof this.state.picOpen[i] === "boolean";
       const isOpen = this.state.picOpen[i];
 
-      const left = isEnter ? 0 : imgBoxWidth * (i % 4);
-      const imgLeft = isEnter ? imgBoxWidth * (i % 4) : 0;
-      const isRight = Math.floor((i % 4) / 2);
-      const isTop = Math.floor(i / 4);
-      let top = isTop ? (isTop - 1) * imgBoxHeight : 0;
+      const left = isEnter ? 0 : imgBoxWidth * (i % 10);
+      const imgLeft = isEnter ? imgBoxWidth * (i % 10) : 0;
+      const isRight = Math.floor((i % 10) / 5);
+      const isTop = Math.floor(i / 10);
+      let top = 0;
       top = isEnter ? top : imgBoxHeight * isTop;
       let imgTop = isTop ? imgBoxHeight : 0;
       imgTop = isEnter ? imgTop : 0;
 
-      const liStyle = isEnter ? { width: '100%', height: 175, zIndex: 1 } : null;
-      const liAnimation = isOpen ?
-        ({ boxShadow: '0 2px 8px rgba(140, 140, 140, .35)' }) :
-        ({ boxShadow: '0 0px 0px rgba(140, 140, 140, 0)' });
-      let aAnimation = isEnter ?
-        ({
-          delay: 400,
-          ease: 'easeInOutCubic',
-          width: imgWidth,
-          height: imgHeight,
-          onComplete: this.onTweenEnd.bind(this, i),
-          left: imgBoxWidth * (i % 4),
-          top: isTop ? imgBoxHeight : 0,
-        }) : null;
-      aAnimation = isOpen ?
-        ({
-          ease: 'easeInOutCubic',
-          left: isRight ? (imgBoxWidth * 2) - 10 : 0,
-          width: '50%',
-          height: 175,
-          top: 0,
-        }) : aAnimation;
+      const liStyle = isEnter
+        ? { width: "100%", height: 680, zIndex: 1 }
+        : null;
+      const liAnimation = isOpen
+        ? { boxShadow: "0 2px 8px rgba(140, 140, 140, .35)" }
+        : { boxShadow: "0 0px 0px rgba(140, 140, 140, 0)" };
+      let aAnimation = isEnter
+        ? {
+            delay: 400,
+            ease: "easeInOutCubic",
+            width: imgWidth,
+            height: imgHeight,
+            onComplete: this.onTweenEnd.bind(this, i),
+            left: imgBoxWidth * (i % 10),
+            top: isTop ? imgBoxHeight : 0,
+          }
+        : null;
+      aAnimation = isOpen
+        ? {
+            ease: "easeInOutCubic",
+            left: isRight ? imgBoxWidth * 5 + 10 : 0,
+            width: "50%",
+            height: 680,
+            top: 0,
+          }
+        : aAnimation;
 
       // 位置 js 控制；
       return (
@@ -135,42 +163,51 @@ class PicDetails extends React.Component {
             ...liStyle,
           }}
           component="li"
-          className={isOpen ? 'open' : ''}
+
+          className={isOpen ? "open" : ""}
           animation={liAnimation}
         >
           <TweenOne
             component="a"
-            onClick={e => this.onImgClick(e, i)}
+            onClick={(e) => this.onImgClick(e, i)}
             style={{
               left: imgLeft,
               top: imgTop,
             }}
             animation={aAnimation}
           >
-            <img src={image} width="100%" height="100%" />
+            <img src={image}  width="100%" height="100%" />
           </TweenOne>
           <TweenOneGroup
             enter={[
               {
-                opacity: 0, duration: 0, type: 'from', delay: 400,
+                opacity: 0,
+                duration: 0,
+                type: "from",
+                delay: 400,
               },
-              { ease: 'easeOutCubic', type: 'from', left: isRight ? '50%' : '0%' },
+              {
+                ease: "easeOutCubic",
+                type: "from",
+                left: isRight ? "50%" : "0%",
+              },
             ]}
-            leave={{ ease: 'easeInOutCubic', left: isRight ? '50%' : '0%' }}
+            leave={{ ease: "easeInOutCubic", left: isRight ? "50%" : "0%" }}
             component=""
           >
             {isOpen && (
               <div
+                onClick={e => this.onClose(e, i)}
                 className={`${this.props.className}-text-wrapper`}
                 key="text"
                 style={{
-                  left: isRight ? '0%' : '50%',
+                  left: isRight ? "0%" : "50%",
+                  height: 680,
                 }}
               >
                 <h1>{title}</h1>
-                {/* <Icon type="cross" onClick={e => this.onClose(e, i)} /> */}
-                <em />
-                <p>{content}</p>
+
+                <div>{content}</div>
               </div>
             )}
           </TweenOneGroup>
@@ -181,33 +218,20 @@ class PicDetails extends React.Component {
 
   render() {
     return (
-      <div>
-        <div className={`${this.props.className}-wrapper`}>
-          <div className={this.props.className}>
-            <div className={`${this.props.className}-header`}>
-              <ul>
-                <li />
-                <li />
-                <li />
-                <li />
-                <li />
-              </ul>
-            </div>
-            <QueueAnim type="bottom" className={`${this.props.className}-title`}>
-              <h1 key="h1">Motion Design</h1>
-              <p key="p">The react animation solution</p>
-            </QueueAnim>
-            <QueueAnim
-              delay={this.getDelay}
-              component="ul"
-              className={`${this.props.className}-image-wrapper`}
-              interval={0}
-              type="bottom"
-            >
-              {this.getLiChildren()}
-            </QueueAnim>
-          </div>
-        </div>
+      <div className={this.props.className}>
+        <QueueAnim type="bottom" className={`${this.props.className}-title`}>
+          <h1 key="h1">国际足球联盟</h1>
+          <p key="p">球星荟萃</p>
+        </QueueAnim>
+        <QueueAnim
+          delay={this.getDelay}
+          component="ul"
+          className={`${this.props.className}-image-wrapper`}
+          interval={0}
+          type="bottom"
+        >
+          {this.getLiChildren()}
+        </QueueAnim>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 /*
  * @Author: shitao
  * @Date: 2021-10-19 21:19:18
- * @LastEditTime: 2021-10-24 12:59:44
+ * @LastEditTime: 2021-12-04 16:31:26
  * @LastEditors: shitao
  * @Description: 
- * @FilePath: \ywhzapp\src\index.js
+ * @FilePath: \starwall\src\index.js
  * 无锡四维时空信息科技有限公司
  */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App.js';
 
 ReactDOM.render(
